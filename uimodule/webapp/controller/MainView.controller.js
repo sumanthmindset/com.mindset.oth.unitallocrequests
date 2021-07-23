8208f1d3896c687fab08052856e999787fdf8fe8
harishmindset <harishkumarchandolu@mindsetconsulting.com>
2021-07-23 19:12:09 +0530

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/mindset/oth/unitallocrequests/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.mindset.oth.unitallocrequests.controller.MainView", {});
+});
